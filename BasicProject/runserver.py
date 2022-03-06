4d428488acd786119160032d31db9675eeebac50
Brian Ukah <ukahb.bu@gmail.com>
2022-03-06 17:01:19 -0500

    Refactor Code

--- BasicProject/runserver.py
+++ BasicProject/runserver.py
@@ -3,20 +3,16 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
+import os
 from flask import Flask
+from HelloFlask import app
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
 
 
-@app.route('/')
-def hello():
-    """Renders a sample page."""
-    return "Hello World!"
-
 if __name__ == '__main__':
-    import os
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(os.environ.get('SERVER_PORT', '5555'))

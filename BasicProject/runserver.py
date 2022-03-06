3bdaeabe364e6e9fd8a6b2e7c7dec16b29c0ddd0
Brian Ukah <ukahb.bu@gmail.com>
2022-03-06 17:09:46 -0500

    Render Template

--- BasicProject/runserver.py
+++ BasicProject/runserver.py
@@ -6,7 +6,7 @@ It contains the definition of routes and views for the application.
 import os
 from flask import Flask
 from HelloFlask import app
-app = Flask(__name__)
+
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app

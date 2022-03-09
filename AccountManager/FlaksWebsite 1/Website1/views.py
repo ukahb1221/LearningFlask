a89bebd8ac7d73159fafe4cc3b54519e506a8957
Brian Ukah <ukahb.bu@gmail.com>
2022-03-08 22:14:54 -0500

    HTML Templates

--- AccountManager/FlaksWebsite 1/Website1/views.py
+++ AccountManager/FlaksWebsite 1/Website1/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint #Import Blueprint to split views
-
+from flask import render_template #Renders html templates created in the sperate files
 views = Blueprint("views", __name__)
 
 @views.route('/') #Binds route to a function
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")

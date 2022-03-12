0dd241490bb9a7d28c4b73e2c273a98e50af8b4d
Brian Ukah <ukahb.bu@gmail.com>
2022-03-12 18:07:15 -0500

    SQL 1

--- AccountManager/FlaksWebsite 1/Website1/__init__.py
+++ AccountManager/FlaksWebsite 1/Website1/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 #import Website1.views
+
+db = SQLAlchemy() #Define a new database
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'asdf'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    db.init_app(app) #Intialize database
 
     from .views import views
     from .auth import auth

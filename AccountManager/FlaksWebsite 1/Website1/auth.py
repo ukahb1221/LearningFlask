8b7075a560687ddc9772dc1159304c7374cbedc4
Brian Ukah <ukahb.bu@gmail.com>
2022-03-16 12:49:35 -0400

    Create Users

--- AccountManager/FlaksWebsite 1/Website1/auth.py
+++ AccountManager/FlaksWebsite 1/Website1/auth.py
@@ -2,6 +2,10 @@ from flask import Blueprint #Import Blueprint to split views
 from flask import render_template #Renders html templates created in the sperate files
 from flask import request #Handles HTTP request
 from flask import flash #Create message handling?
+from flask import redirect
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from .import db
 
 auth = Blueprint("auth", __name__)
 
@@ -20,13 +24,13 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get("email")
-        firstName = request.form.get("firstName")
+        first_name = request.form.get("firstName")
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
 
         if len(email) < 4:
             flash("Email must be greater than 3 characters.", category="error")
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash("First name must be greater than 1 character.", category="error")
         elif password1 != password2:
            flash("Paswords don't match.", category="error")
@@ -34,6 +38,10 @@ def sign_up():
             flash("Pasword must be at least 7 characters.", category="error")
         else:
             #Add the user
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash("Account created.", category="success")
+            return redirect(url_for("views.home"))
 
     return render_template("sign_up.html")
